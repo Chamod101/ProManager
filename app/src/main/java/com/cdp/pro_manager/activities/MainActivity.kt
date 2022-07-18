ea924ca45e2391cbf8379ec937875027aabea48a
Chamod Dilushanka <chamoddperera99@gmail.com>
2022-07-18 08:57:26 +0530

    Initial commit

---
+++ app/src/main/java/com/cdp/pro_manager/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cdp.pro_manager.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cdp.pro_manager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
